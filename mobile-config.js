cc175038ad431d81c53e928e65e2c2068131bc1e
Nick P Smith <npsmith1990@gmail.com>
2015-08-17 15:41:45 -0500

    add mobile config file

---
+++ mobile-config.js
@@ -0,0 +1,34 @@
+App.info({
+  id: 'com.nicholaspsmith.pushups',
+  version: '0.0.1',
+  name: 'Pushups',
+  description: 'Track number of pushups, pullups, and situps you do in a day.',
+  author: 'Nicholas P Smith',
+  email: 'me@nicholaspsmith.com',
+  website: 'http://nicholaspsmith.com'
+});
+
+App.icons({
+  // 'iphone': 'icons/icon-120x120.png',
+  // 'iphone_2x': 'icons/icon-120x120.png',
+  // 'iphone_3x': 'icons/icon-180x180.png',
+  // 'ipad': 'icons/icon-76x76.png',
+  // 'ipad_2x': 'icons/icon-152x152.png',
+});
+
+App.launchScreens({
+  //  'iphone': 'splash/mainsplash.png',
+  //  'iphone_2x': 'splash/mainsplash.png',
+  //  'iphone5': 'splash/mainsplash.png',
+  //  'ipad_portrait': 'splash/mainsplash.png',
+  //  'ipad_portrait_2x': 'splash/mainsplash.png',
+  //  'ipad_landscape': 'splash/mainsplash.png',
+  //  'ipad_landscape_2x': 'splash/mainsplash.png'
+});
+
+// Set PhoneGap/Cordova preferences
+App.setPreference('StatusBarOverlaysWebView', 'false');
+App.setPreference('StatusBarBackgroundColor', '#000');
+App.setPreference('ShowSplashScreenSpinner', 'true');
+
+App.accessRule('*');
