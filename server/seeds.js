a98409bc942215c2524d6fc5c0b94964de936d66
Nick P Smith <npsmith1990@gmail.com>
2015-09-01 09:54:05 -0600

    seed this past week

--- server/seeds.js
+++ server/seeds.js
@@ -1,135 +1,79 @@
 if (Movements.find().count() === 0) {
-  var aug10 = new Date('8/10/2015 8:15');
-  var aug11 = new Date('8/11/2015 8:15');
-  var aug12 = new Date('8/12/2015 8:15');
-  var aug13 = new Date('8/13/2015 8:15');
-  var aug14 = new Date('8/14/2015 8:15');
-  var aug15 = new Date('8/15/2015 8:15');
-  var aug16 = new Date('8/16/2015 8:15');
-  var aug17 = new Date('8/17/2015 8:15');
-  var aug18 = new Date('8/18/2015 8:15');
-
-  Movements.insert({
-    type: 'pushups',
-    quantity: 5,
-    date: aug10
-  });
-  Movements.insert({
-    type: 'pushups',
-    quantity: 12,
-    date: aug10
-  });
-  Movements.insert({
-    type: 'pullups',
-    quantity: 5,
-    date: aug10
-  });
-  Movements.insert({
-    type: 'situps',
-    quantity: 2,
-    date: aug10
-  });
-  Movements.insert({
-    type: 'pullups',
-    quantity: 25,
-    date: aug10
-  });
-  Movements.insert({
-    type: 'squats',
-    quantity: 25,
-    date: aug10
-  });
-
-
-
-  Movements.insert({
-    type: 'pushups',
-    quantity: 3,
-    date: aug11
-  });
-  Movements.insert({
-    type: 'situps',
-    quantity: 5,
-    date: aug11
-  });
-  Movements.insert({
-    type: 'pullups',
-    quantity: 9,
-    date: aug11
-  });
-  Movements.insert({
-    type: 'pullups',
-    quantity: 5,
-    date: aug11
-  });
+  var day1 = new Date('8/26/2015 8:15');
+  var day2 = new Date('8/27/2015 8:15');
+  var day3 = new Date('8/28/2015 8:15');
+  var day4 = new Date('8/29/2015 8:15');
+  var day5 = new Date('8/30/2015 8:15');
+  var day6 = new Date('8/31/2015 8:15');
+  var day7 = new Date('9/1/2015 8:15');
 
 
   Movements.insert({
     type: 'pullups',
     quantity: 30,
-    date: aug12
+    date: day1
   });
   Movements.insert({
     type: 'pushups',
     quantity: 12,
-    date: aug12
+    date: day1
   });
   Movements.insert({
     type: 'situps',
     quantity: 9,
-    date: aug12
+    date: day1
   });
   Movements.insert({
     type: 'pushups',
     quantity: 5,
-    date: aug12
+    date: day1
   });
   Movements.insert({
     type: 'pullups',
     quantity: 5,
-    date: aug12
+    date: day1
   });
   Movements.insert({
     type: 'situps',
     quantity: 5,
-    date: aug12
+    date: day1
   });
 
 
   Movements.insert({
     type: 'pullups',
     quantity: 10,
-    date: aug13
+    date: day2
   });
   Movements.insert({
     type: 'pushups',
     quantity: 12,
-    date: aug13
+    date: day2
   });
   Movements.insert({
     type: 'situps',
     quantity: 10,
-    date: aug13
+    date: day2
   });
   Movements.insert({
     type: 'pushups',
     quantity: 10,
-    date: aug13
+    date: day2
   });
   Movements.insert({
     type: 'pullups',
     quantity: 10,
-    date: aug13
+    date: day2
   });
   Movements.insert({
     type: 'situps',
     quantity: 10,
-    date: aug13
+    date: day2
   });
   Movements.insert({
     type: 'squats',
     quantity: 10,
-    date: aug13
+    date: day2
   });
 
 
@@ -137,154 +81,154 @@ if (Movements.find().count() === 0) {
   Movements.insert({
     type: 'pullups',
     quantity: 14,
-    date: aug14
+    date: day3
   });
   Movements.insert({
     type: 'pushups',
     quantity: 14,
-    date: aug14
+    date: day3
   });
   Movements.insert({
     type: 'situps',
     quantity: 14,
-    date: aug14
+    date: day3
   });
   Movements.insert({
     type: 'pushups',
     quantity: 14,
-    date: aug14
+    date: day3
   });
   Movements.insert({
     type: 'pullups',
     quantity: 14,
-    date: aug14
+    date: day3
   });
   Movements.insert({
     type: 'situps',
     quantity: 14,
-    date: aug14
+    date: day3
   });
 
 
   Movements.insert({
     type: 'pullups',
     quantity: 18,
-    date: aug15
+    date: day4
   });
   Movements.insert({
     type: 'pushups',
     quantity: 12,
-    date: aug15
+    date: day4
   });
   Movements.insert({
     type: 'situps',
     quantity: 3,
-    date: aug15
+    date: day4
   });
   Movements.insert({
     type: 'pushups',
     quantity: 9,
-    date: aug15
+    date: day4
   });
   Movements.insert({
     type: 'pullups',
     quantity: 12,
-    date: aug15
+    date: day4
   });
   Movements.insert({
     type: 'situps',
     quantity: 25,
-    date: aug15
+    date: day4
   });
 
 
   Movements.insert({
     type: 'pullups',
     quantity: 7,
-    date: aug16
+    date: day5
   });
   Movements.insert({
     type: 'pushups',
     quantity: 12,
-    date: aug16
+    date: day5
   });
   Movements.insert({
     type: 'situps',
     quantity: 20,
-    date: aug16
+    date: day5
   });
   Movements.insert({
     type: 'pushups',
     quantity: 8,
-    date: aug16
+    date: day5
   });
   Movements.insert({
     type: 'pullups',
     quantity: 9,
-    date: aug16
+    date: day5
   });
   Movements.insert({
     type: 'situps',
     quantity: 12,
-    date: aug16
+    date: day5
   });
 
 
   Movements.insert({
     type: 'pullups',
     quantity: 20,
-    date: aug17
+    date: day6
   });
   Movements.insert({
     type: 'pushups',
     quantity: 14,
-    date: aug17
+    date: day6
   });
   Movements.insert({
     type: 'situps',
     quantity: 10,
-    date: aug17
+    date: day6
   });
   Movements.insert({
     type: 'pushups',
     quantity: 3,
-    date: aug17
+    date: day6
   });
   Movements.insert({
     type: 'pullups',
     quantity: 2,
-    date: aug17
+    date: day6
   });
   Movements.insert({
     type: 'situps',
     quantity: 2,
-    date: aug17
+    date: day6
   });
 
 
   Movements.insert({
     type: 'pushups',
     quantity: 20,
-    date: aug18
+    date: day7
   });
   Movements.insert({
     type: 'situps',
     quantity: 20,
-    date: aug18
+    date: day7
   });
   Movements.insert({
     type: 'pushups',
     quantity: 20,
-    date: aug18
+    date: day7
   });
   Movements.insert({
     type: 'pullups',
     quantity: 30,
-    date: aug18
+    date: day7
   });
   Movements.insert({
     type: 'squats',
     quantity: 30,
-    date: aug18
+    date: day7
   });
 }
