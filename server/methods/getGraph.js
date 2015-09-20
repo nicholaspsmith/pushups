b61ab65668d0aa6522599819da85ab28f26eb983
Nick P Smith <npsmith1990@gmail.com>
2015-09-20 14:28:22 -0500

    add method for getting movements from single day

--- server/methods/getGraph.js
+++ server/methods/getGraph.js
@@ -30,6 +30,35 @@ Meteor.methods({
     ];
     var res = Movements.aggregate(pipeline);
     return res;
+  },
+  getSingleDayGraph: function() {
+    var pipeline = [
+      {
+        $match: {
+          date: {
+            $gte: moment().startOf('day').toDate(),
+            $lte: moment().endOf('day').toDate()
+          }
+        }
+      },
+      {
+        $group: {
+          _id: {type: '$type', day: {$dayOfMonth: '$date'}},
+          count: {$sum: '$quantity'}
+        }
+      },
+      {
+        $group: {
+          _id: '$_id.day',
+          counts: {$push: {
+            type: '$_id.type',
+            count: '$count'
+          }}
+        }
+      }
+    ];
+    var res = Movements.aggregate(pipeline);
+    return res;
   }
 });
 
