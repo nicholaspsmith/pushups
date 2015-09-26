651cee78a86fdc97813daf2899c69f4b7593d04e
Nick P Smith <npsmith1990@gmail.com>
2015-09-26 14:07:30 -0500

    comment out seeds file

--- server/seeds.js
+++ server/seeds.js
@@ -1,236 +1,233 @@
-  console.log('not seeding');
-if (Movements.find().count() === 0) {
-  console.log('seeding');
-  var day1 = new Date('9/20/2015 8:15');
-  // var day2 = new Date('8/27/2015 8:15');
-  // var day3 = new Date('8/28/2015 8:15');
-  // var day4 = new Date('8/29/2015 8:15');
-  // var day5 = new Date('8/30/2015 8:15');
-  // var day6 = new Date('8/31/2015 8:15');
-  // var day7 = new Date('9/1/2015 8:15');
-
-
-  Movements.insert({
-    type: 'pullups',
-    quantity: 30,
-    date: day1
-  });
-  Movements.insert({
-    type: 'pushups',
-    quantity: 12,
-    date: day1
-  });
-  Movements.insert({
-    type: 'situps',
-    quantity: 9,
-    date: day1
-  });
-  Movements.insert({
-    type: 'pushups',
-    quantity: 5,
-    date: day1
-  });
-  Movements.insert({
-    type: 'pullups',
-    quantity: 5,
-    date: day1
-  });
-  Movements.insert({
-    type: 'situps',
-    quantity: 5,
-    date: day1
-  });
-
-  //
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 10,
-  //   date: day2
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 12,
-  //   date: day2
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 10,
-  //   date: day2
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 10,
-  //   date: day2
-  // });
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 10,
-  //   date: day2
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 10,
-  //   date: day2
-  // });
-  // Movements.insert({
-  //   type: 'squats',
-  //   quantity: 10,
-  //   date: day2
-  // });
-  //
-  //
-  //
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 14,
-  //   date: day3
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 14,
-  //   date: day3
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 14,
-  //   date: day3
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 14,
-  //   date: day3
-  // });
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 14,
-  //   date: day3
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 14,
-  //   date: day3
-  // });
-  //
-  //
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 18,
-  //   date: day4
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 12,
-  //   date: day4
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 3,
-  //   date: day4
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 9,
-  //   date: day4
-  // });
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 12,
-  //   date: day4
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 25,
-  //   date: day4
-  // });
-  //
-  //
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 7,
-  //   date: day5
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 12,
-  //   date: day5
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 20,
-  //   date: day5
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 8,
-  //   date: day5
-  // });
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 9,
-  //   date: day5
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 12,
-  //   date: day5
-  // });
-  //
-  //
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 20,
-  //   date: day6
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 14,
-  //   date: day6
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 10,
-  //   date: day6
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 3,
-  //   date: day6
-  // });
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 2,
-  //   date: day6
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 2,
-  //   date: day6
-  // });
-  //
-  //
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 20,
-  //   date: day7
-  // });
-  // Movements.insert({
-  //   type: 'situps',
-  //   quantity: 20,
-  //   date: day7
-  // });
-  // Movements.insert({
-  //   type: 'pushups',
-  //   quantity: 20,
-  //   date: day7
-  // });
-  // Movements.insert({
-  //   type: 'pullups',
-  //   quantity: 30,
-  //   date: day7
-  // });
-  // Movements.insert({
-  //   type: 'squats',
-  //   quantity: 30,
-  //   date: day7
-  // });
-}
+// if (Movements.find().count() === 0) {
+//   var day1 = new Date();
+//   var day2 = moment().subtract(1,'day').toDate();
+//   var day3 = moment().subtract(2,'day').toDate();
+//   var day4 = moment().subtract(3,'day').toDate();
+//   var day5 = moment().subtract(4,'day').toDate();
+//   var day6 = moment().subtract(5,'day').toDate();
+//   var day7 = moment().subtract(6,'day').toDate();
+//
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 30,
+//     date: day1
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 12,
+//     date: day1
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 9,
+//     date: day1
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 5,
+//     date: day1
+//   });
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 5,
+//     date: day1
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 5,
+//     date: day1
+//   });
+//
+//
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 10,
+//     date: day2
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 12,
+//     date: day2
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 10,
+//     date: day2
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 10,
+//     date: day2
+//   });
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 10,
+//     date: day2
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 10,
+//     date: day2
+//   });
+//   Movements.insert({
+//     type: 'squats',
+//     quantity: 10,
+//     date: day2
+//   });
+//
+//
+//
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 14,
+//     date: day3
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 14,
+//     date: day3
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 14,
+//     date: day3
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 14,
+//     date: day3
+//   });
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 14,
+//     date: day3
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 14,
+//     date: day3
+//   });
+//
+//
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 18,
+//     date: day4
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 12,
+//     date: day4
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 3,
+//     date: day4
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 9,
+//     date: day4
+//   });
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 12,
+//     date: day4
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 25,
+//     date: day4
+//   });
+//
+//
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 7,
+//     date: day5
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 12,
+//     date: day5
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 20,
+//     date: day5
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 8,
+//     date: day5
+//   });
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 9,
+//     date: day5
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 12,
+//     date: day5
+//   });
+//
+//
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 20,
+//     date: day6
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 14,
+//     date: day6
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 10,
+//     date: day6
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 3,
+//     date: day6
+//   });
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 2,
+//     date: day6
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 2,
+//     date: day6
+//   });
+//
+//
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 20,
+//     date: day7
+//   });
+//   Movements.insert({
+//     type: 'situps',
+//     quantity: 20,
+//     date: day7
+//   });
+//   Movements.insert({
+//     type: 'pushups',
+//     quantity: 20,
+//     date: day7
+//   });
+//   Movements.insert({
+//     type: 'pullups',
+//     quantity: 30,
+//     date: day7
+//   });
+//   Movements.insert({
+//     type: 'squats',
+//     quantity: 30,
+//     date: day7
+//   });
+// }
