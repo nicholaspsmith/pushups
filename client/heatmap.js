55b5054867e8adc323fd889d0042f1962c37f7ef
Nick P Smith <npsmith1990@gmail.com>
2015-09-26 00:22:09 -0500

    move array sorting logic into a meteor method

--- client/heatmap.js
+++ client/heatmap.js
@@ -1,104 +1,53 @@
 Session.setDefault("movementArray", []);
 Template.heatmap.onRendered(function() {
-  window.movementArray = [];
-  Meteor.call('getGraph',Session.get('currentDate'), function(err,res) {
-    if (err) {
-      console.log(err.message);
-    } else {
-      // [date, type, number]
-      res.forEach(function(item){
-        item.counts.forEach(function(count){
-          var miniarr = [];
-          if (count.type === 'pushups') {
-            miniarr.push(item._id);
-            miniarr.push(0);
-            miniarr.push(count.count);
-          } else if (count.type === 'situps') {
-            miniarr.push(item._id);
-            miniarr.push(1);
-            miniarr.push(count.count);
-          } else if (count.type === 'pullups') {
-            miniarr.push(item._id);
-            miniarr.push(2);
-            miniarr.push(count.count);
-          } else if (count.type === 'squats') {
-            miniarr.push(item._id);
-            miniarr.push(3);
-            miniarr.push(count.count);
-          }
-          movementArray.push(miniarr);
-          movementArray.sort(compareSecondColumn);
-          function compareSecondColumn(a, b) {
-            if (a[1] === b[1]) {
-              return 0;
-            }
-            else {
-              return (a[1] < b[1]) ? -1 : 1;
-            }
-          }
-        });
-
-        movementArray.sort(sortFunction);
-
-        function sortFunction(a, b) {
-          if (a[0] === b[0]) {
-            return 0;
-          }
-          else {
-            return (a[0] < b[0]) ? -1 : 1;
-          }
-        }
-      });
-      Session.set('movementArray',movementArray);
-    }
+  Meteor.call('getArrayGraph',Session.get('currentDate'), function(err,res) {
+    Session.set('movementArray', res);
   });
-
 });
 
-Template.heatmap.topGenresChart = function() {
-  // while (Session.get('movementArray').length === 0) {
-    // setTimeout(function(){},200);
-  // }
-  return {
-    chart: {
-      type: 'heatmap',
-      marginTop: 40,
-      marginBottom: 80,
-      plotBorderWidth: 1
-    },
-    title: {
-      text: 'Number of Exercises by Day'
-    },
-    yAxis: {
-      categories: ['Pushups', 'Situps', 'Pullups', 'Squats'],
-      title: null
-    },
-    colorAxis: {
-      min: 0,
-      minColor: '#FFFFFF',
-      maxColor: Highcharts.getOptions().colors[0]
-    },
-    legend: {
-      align: 'right',
-      layout: 'vertical',
-      margin: 0,
-      verticalAlign: 'top',
-      y: 25,
-      symbolHeight: 280
-    },
-    tooltip: {
-      formatter: function () {
-        return  this.point.value + ' ' + this.series.yAxis.categories[this.point.y];
-      }
-    },
-    series: [{
-      name: 'Sales per employee',
-      borderWidth: 1,
-      data: Session.get('movementArray'),
-      dataLabels: {
-        enabled: true,
-        color: '#000000'
-      }
-    }]
-  };
-};
+Template.heatmap.helpers({
+  movementChart: function () {
+    return {
+      chart: {
+        type: 'heatmap',
+        marginTop: 40,
+        marginBottom: 80,
+        plotBorderWidth: 1
+      },
+      title: {
+        text: 'Number of Exercises by Day'
+      },
+      yAxis: {
+        categories: ['Pushups', 'Situps', 'Pullups', 'Squats'],
+        title: null
+      },
+      colorAxis: {
+        min: 0,
+        minColor: '#FFFFFF',
+        maxColor: Highcharts.getOptions().colors[0]
+      },
+      legend: {
+        align: 'right',
+        layout: 'vertical',
+        margin: 0,
+        verticalAlign: 'top',
+        y: 25,
+        symbolHeight: 280
+      },
+      tooltip: {
+        formatter: function () {
+          return  this.point.value + ' ' + this.series.yAxis.categories[this.point.y];
+        }
+      },
+      series: [{
+        name: 'Sales per employee',
+        borderWidth: 1,
+        data: dataArr,
+        dataLabels: {
+          enabled: true,
+          color: '#000000'
+        }
+      }]
+    };
+  }
+});
--- server/methods/getGraph.js
+++ server/methods/getGraph.js
@@ -32,6 +32,69 @@ Meteor.methods({
     var res = Movements.aggregate(pipeline);
     return res;
   },
+  getArrayGraph: function(date) {
+    var daystart = moment(date).startOf('day').subtract(7,'days').startOf('day').toDate();
+    var daysmovements = lookForMovements(7, date);
+    var daystop = moment(date).startOf('day').endOf('day').toDate();
+    var pipeline = [
+      {
+        $match: {
+          // user: this.userId,
+          date: {
+            $gte: daystart,
+            $lte: daystop
+          }
+        }
+      },
+      {
+        $group: {
+          _id: {type: '$type', day: {$dayOfMonth: '$date'}},
+          count: {$sum: '$quantity'}
+        }
+      },
+      {
+        $group: {
+          _id: '$_id.day',
+          counts: {$push: {
+            type: '$_id.type',
+            count: '$count'
+          }}
+        }
+      }
+    ];
+    var res = Movements.aggregate(pipeline);
+    var movementArray = [];
+    res.forEach(function(item){
+      item.counts.forEach(function(count){
+        var miniarr = [];
+        if (count.type === 'pushups') {
+          miniarr.push(item._id - 1);
+          miniarr.push(0);
+          miniarr.push(count.count);
+        } else if (count.type === 'situps') {
+          miniarr.push(item._id - 1);
+          miniarr.push(1);
+          miniarr.push(count.count);
+        } else if (count.type === 'pullups') {
+          miniarr.push(item._id - 1);
+          miniarr.push(2);
+          miniarr.push(count.count);
+        } else if (count.type === 'squats') {
+          miniarr.push(item._id - 1);
+          miniarr.push(3);
+          miniarr.push(count.count);
+        }
+        movementArray.push(miniarr);
+      });
+    });
+    movementArray.sort(function(a,b) {
+      if (a[0] == b[0])
+        return a[1] < b[1] ? -1 : 1;
+      return a[0] < b[0] ? -1 : 1;
+    });
+    
+    return movementArray;
+  },
   getSingleDayGraph: function(date) {
     var pipeline = [
       {
